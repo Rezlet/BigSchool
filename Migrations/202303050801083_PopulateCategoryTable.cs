86896cd9802d2943fc8c3a7706efa69f539e75e9
Mr.Mistake <thanhtrollvt02@gmail.com>
2023-03-05 15:40:53 +0700

    Add project files.

---
+++ Migrations/202303050801083_PopulateCategoryTable.cs
@@ -0,0 +1,21 @@
+﻿namespace BigSchool.Migrations
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Data.Entity.Migrations;
+    
+    public partial class PopulateCategoryTable : DbMigration
+    {
+        public override void Up()
+        {
+            Sql("SET IDENTITY_INSERT  dbo.Categories  ON");
+            Sql("INSERT INTO CATEGORIES (Id, Name) VALUES (1,'Development')");
+            Sql("INSERT INTO CATEGORIES (Id, Name) VALUES (2,'Business')");
+            Sql("INSERT INTO CATEGORIES (Id, Name) VALUES (3,'Marketing')");
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
